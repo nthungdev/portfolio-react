fa38ead1ad10720948791b8894e1bfeaefb54b78
Hung Nguyen <nthugndev@gmail.com>
2019-02-21 00:57:47 -0500

    update on header

---
+++ src/components/NavButton/NavButton.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./NavButton.css";
+
+class NavButton extends Component {
+  render() {
+    return (
+      <a
+        className={
+          this.props.isDisabled
+            ? "nav-button disabled fade-out"
+            : "nav-button fade-in"
+        }
+        onClick={this.props.onClick}
+        onMouseOver={this.props.onMouseOver}
+        onMouseOut={this.props.onMouseOut}
+        href={this.props.href}
+      >
+        {this.props.text}
+      </a>
+    );
+  }
+}
+
+NavButton.defaultProps = {
+  isDisabled: false
+};
+
+NavButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired
+};
+
+export default NavButton;
--- src/sections/HeaderSection/HeaderSection.js
+++ src/sections/HeaderSection/HeaderSection.js
@@ -1,24 +1,125 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PageLogo from "../../assets/logos/logo.png";
+import NavButton from "../../components/NavButton/NavButton";
+import "./HeaderSection.css";
 
 class HeaderSection extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+      buttons: [
+        {
+          text: "Home",
+          className: "nav-button",
+          isDisabled: false,
+          href: "#"
+        },
+        {
+          text: "Experience",
+          className: "nav-button",
+          disabled: false,
+          href: "#"
+        },
+        {
+          text: "Photography",
+          className: "nav-button",
+          disabled: false,
+          href: "#"
+        },
+        {
+          text: "Contact",
+          className: "nav-button",
+          disabled: false,
+          href: "#"
+        }
+      ]
+    };
+    this.onHover = this.onMouseOver.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onMouseOver(button) {
+    const buttons = [...this.state.buttons];
+    buttons.forEach(item => {
+      if (item.text !== button.text) {
+        item.isDisabled = true;
+      }
+    });
+    // });
+    this.setState({
+      buttons: buttons
+    });
+  }
+
+  onMouseOut(button) {
+    const buttons = [...this.state.buttons];
+    buttons.forEach(item => {
+      item.isDisabled = false;
+    });
+    this.setState({
+      buttons: buttons
+    });
+  }
+
+  onClick() {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn
+    });
+  }
+
   render() {
     return (
-        <header className="header-primary">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header className="header-section-primary">
+          <div className="logo">
+            <img src={PageLogo} alt="Logo of the page" />
+          </div>
+          <nav className="navbar">
+            {this.state.buttons.map(button => (
+              <NavButton
+                className={button.className}
+                text={button.text}
+                href={button.href}
+                isDisabled={button.isDisabled}
+                onMouseOver={() => this.onMouseOver(button)}
+                onMouseOut={() => this.onMouseOut(button)}
+                onClick={this.onClick}
+              />
+            ))}
+            {/* <NavButton
+              className="nav-button"
+              onMouseOver={() => this.onHover(this.state.buttons.homeButton)}
+              onClick={this.onClick}
+              text="Home"
+              href="#"
+              disabled={this.state.buttons.homeButton}
+            />
+            <NavButton
+              className="nav-button"
+              text="Experience"
+              href="#"
+              disabled={this.state.buttons.experienceButton}
+            />
+            <NavButton
+              className="nav-button"
+              text="Photography"
+              href="#"
+              disabled={this.state.buttons.photographyButton}
+            />
+            <NavButton
+              className="nav-button"
+              text="Contact"
+              href="https://www.google.com/"
+              disabled={this.state.buttons.contactButton}
+            /> */}
+          </nav>
         </header>
+
+        <h1>{this.state.isToggleOn ? "on" : "off"}</h1>
+      </div>
     );
   }
 }
 
-export default App;
+export default HeaderSection;
