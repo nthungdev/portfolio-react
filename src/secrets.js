1771630798b29706d029a8f51bcc4148cb19cf66
Hung Nguyen <nthungdev@gmail.com>
2019-03-15 15:45:48 -0400

    reveal secret

---
+++ src/secrets.js
@@ -0,0 +1,8 @@
+export var firebaseConfig = {
+  apiKey: "AIzaSyA28aqTIoDjsjISwehCWIgFPcoa2L-EXgo",
+  authDomain: "my-portfolio-nthungdev.firebaseapp.com",
+  databaseURL: "https://my-portfolio-nthungdev.firebaseio.com",
+  projectId: "my-portfolio-nthungdev",
+  storageBucket: "my-portfolio-nthungdev.appspot.com",
+  messagingSenderId: "995220339944"
+};
