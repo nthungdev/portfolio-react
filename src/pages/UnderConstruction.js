ee814007a799aafc275414a1fab87decd5c93297
Hung Nguyen <nthungdev@gmail.com>
2023-09-28 17:10:46 -0400

    hide old site from landing

--- src/pages/UnderConstruction.js
+++ src/pages/UnderConstruction.js
@@ -5,9 +5,10 @@ const UnderConstruction = () => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="text-center">
-        <h1 className="mb-4">My site is under construction :)</h1>
+        <h1 className="mb-4 text-3xl">My site is under construction 😊</h1>
+        <p>Thank you for your interest though. <br/> Please come back another time!</p>
 
-        <Link to="/deprecated" className="btn">
+        <Link to="/deprecated" className="btn hidden">
           See my old site
         </Link>
       </div>
